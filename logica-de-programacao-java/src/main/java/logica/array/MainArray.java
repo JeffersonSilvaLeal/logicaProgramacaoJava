470a17d820512677da9ad9776fed3518f58e0814
Jefferson <drankhar@gmail.com>
2021-04-16 18:28:54 -0300

    nesta parte aprendemos o básico dos arrays, usando um array na prática para receber notas de um aluno, e usando uma estrutura de repetição para facilitar o processo de recebimento dos dados!!

---
+++ logica-de-programacao-java/src/main/java/logica/array/MainArray.java
@@ -0,0 +1,29 @@
+package logica.array;
+
+import java.net.MulticastSocket;
+import java.util.Scanner;
+
+// array ou vetor são um agrupador de dados
+public class MainArray {
+
+	public static void main(String[] args) {
+		
+		Scanner ler = new Scanner(System.in);
+	//  Array de notas	       Tamanho do array [0, 1, 2], 3 Elementos
+		double[] notas = new double[4];
+	
+	// Armazena dentro do array as notas	
+		for (int i = 0; i < notas.length; i++) {
+			System.out.println("Digite a nota " + (i + 1) + " : "); // Incrementa a nota requisitada ao usuário
+			notas[i] = ler.nextDouble(); // adiciona a nota dentro do array
+		}
+	
+	//   Imprime as notas do array
+		for (int i = 0; i < notas.length; i++) {
+			System.out.println(notas[i]);
+		}
+	// Fecha o scanner	
+		ler.close();
+	}
+	
+}
